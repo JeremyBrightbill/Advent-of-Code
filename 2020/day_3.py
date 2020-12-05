bd89866a25603f71d6b554182ae75f18b4fd5f26
Jeremy Brightbill <jbrightbill@lasallenetwork.com>
2020-12-05 12:47:49 -0600

    Solved Day 3, revised template

--- 2020/day_3.py
+++ 2020/day_3.py
@@ -1,45 +1,54 @@
+import math
 from utilities import load_data
 
+DAY: int = 3
+
 # Import -------------------------------------
+
+# Puzzle input
+
+data = load_data(2020, DAY)
 
 # Sample data
 
 #        NOT AVAILABLE FOR THIS ONE
 
-# Puzzle input
-
-data = load_data(2020, 3)
-
 # Solve part 1 -------------------------------
 
-def tree_or_open(line: str, index: int) -> str:
-    return line[index]
+def traverse_map(map: list, slope_right: int, slope_down: int) -> int: 
 
-# print(tree_or_open(data[0], 2))
-
-def traverse_map(map: list, slope_x: int, slope_y: int) -> list: 
-
-    x: int = 0
-    y: int = 0
+    right: int = 0
+    down: int = 0
     output: list = []
 
     # Find width of input map
     width_list: list = list(set([len(row) for row in map]))
     width: int = int(width_list[0])
 
-    while y < len(map): 
+    while down < len(map): 
 
-        position = map[y][x]
+        position: str = map[down][right]
         output.append(position)
-        x += slope_x
-        y -= slope_y
+        right += slope_right
+        down += slope_down
 
-        if x >= width: 
-            x = x - width
+        if right >= width: 
+            right = right - width
 
-    return output
-
-findings: list = traverse_map(data, slope_x = 3, slope_y = -1)
-print(findings.count('#'))
+    return output.count('#')
 
 # Solve Part 2 -------------------------------
+
+rights: list = [1, 3, 5, 7, 1]
+downs: list = [1, 1, 1, 1, 2]
+
+
+# Output -------------------------------------
+
+if __name__ == "__main__": 
+
+    solution_1: int = traverse_map(data, 3, 1)
+    print(f'Day {DAY} Part 1: {solution_1}')
+
+    solution_2: int = math.prod([traverse_map(data, right, down) for right, down in zip(rights, downs)])
+    print(f'Day {DAY} Part 2: {solution_2}')
--- 2020/template.py
+++ 2020/template.py
@@ -1,17 +1,33 @@
-import sys
 from utilities import *
+
+DAY: int = 3
 
 # Import -------------------------------------
 
+# Puzzle input
+
+data = load_data(2020, DAY)
+
 # Sample data
 
-
-# Puzzle input
 
 
 # Solve part 1 -------------------------------
 
 
 
-
 # Solve Part 2 -------------------------------
+
+
+
+# Output -------------------------------------
+
+if __name__ == "__main__": 
+    
+    print("Test")
+
+    # solution_1 = 
+    # print(f'Part 1: {solution_1}')
+
+    # solution_2 = 
+    # print(f'Part 2: {solution_2}')
