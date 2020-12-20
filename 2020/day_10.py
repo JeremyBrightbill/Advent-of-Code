0ae7e688518416539d1432591d452d354a44032d
Jeremy Brightbill <jbrightbill@lasallenetwork.com>
2020-12-20 09:10:35 -0600

    Working on Day 10

--- 2020/day_10.py
+++ 2020/day_10.py
@@ -1,13 +1,19 @@
 """Title"""
 DAY: int = 10
 
-from utilities import load_data
+# from utilities import load_data
+# from os import listdir
 
 # Import -------------------------------------
 
+def load_data(day: int) -> list:
+    path: str = f'data/{str(day)}.txt'
+    with open(path) as f:
+        data = f.readlines()
+    return [element.strip() for element in data] # remove \n characters
 # Puzzle input
-# data_raw = [int(item) for item in load_data(2020, DAY)]
-# data = [0] + sorted(data_raw) + [max(data_raw) + 3]
+data_raw = [int(item) for item in load_data(DAY)]
+data = [0] + sorted(data_raw) + [max(data_raw) + 3]
 
 # Sample data
 samp1_raw: list = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
@@ -26,30 +32,87 @@ def list_differences(x: list) -> list:
 
     return output
 
-#pt1: list = list_differences(data)
+pt1: list = list_differences(data)
     
 # Solve Part 2 -------------------------------
 
-def find_omissible(x: list) -> list:
+# def find_omissible(x: list) -> list:
+
+#     # Make sure sorted
+#     x = sorted(x)
+#     output: list = []
+
+#     for i, item in enumerate(x[:-1]):
+#         if i > 0 and x[i+1] - x[i-1] <= 3: 
+#             output.append(item)
+    
+#     return output
+
+# omissible: list = find_omissible(samp1)
+
+# def apply_omissions(x: list, omissible_list: list) -> list:
+
+#     output: list = []
+
+#     for item in omissible_list: 
+#         copy: list = x.copy()
+#         del copy[item]
+#         output.append(copy)
+
+#     return output
+
+# samp2: list = apply_omissions(samp1, omissible)
+
+
+def omit_where_possible(x: list) -> list:
+    """Function that takes an arrangement (x), determines which items are omissible, 
+    and returns a list of all possible new arrangements"""
 
     # Make sure sorted
     x = sorted(x)
+    omissible: list = []
+
+    for i, item in enumerate(x[:-1]): # MUST BE A PROBLEM HERE!!!
+        if i > 0 and x[i+1] - x[i-1] <= 3: 
+            omissible.append(item)
+    
     output: list = []
 
-    for i, item in enumerate(x[:-1]):
-        if i > 0 and x[i+1] - x[i-1] <= 3: 
-            output.append(item)
-    
+    for item in omissible: 
+        copy: list = x.copy()
+        copy.remove(item)
+        output.append(copy)
+
     return output
 
-test1: list = find_omissible(samp1)
+# THIS IS ON THE RIGHT TRACK, just figure out how to keep track of new values. 
+# Then simplify
+
+samp2: list = omit_where_possible(samp1)
+
+samp3: list = []
+
+for item in samp2: 
+    omitted: list = omit_where_possible(item)
+    if len(omitted) > 0: 
+        for sublist in omitted: 
+            samp3.append(sublist)
+
+#samp4: list = []
+
+for item in samp3: 
+    if item not in samp2: 
+        samp2.append(item)
+
+
 
 # Output -------------------------------------
 
 if __name__ == "__main__": 
     
-    # solution_1: int = pt1.count(1) * pt1.count(3)
-    # print(f'Part 1: {solution_1}')
+    solution_1: int = pt1.count(1) * pt1.count(3)
+    print(f'Part 1: {solution_1}')
 
-    solution_2: list = test1
-    print(f'Part 2: {solution_2}')
+    print(samp2)
+    # solution_2: list = test1
+    # print(f'Part 2: {solution_2}')
