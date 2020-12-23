f77533f76c85638560d210d9609497f565367a5a
Jeremy Brightbill <jbrightbill@lasallenetwork.com>
2020-12-23 13:55:58 -0600

    Solved Day 10 Part 2

--- 2020/day_10.py
+++ 2020/day_10.py
@@ -1,8 +1,9 @@
-"""Title"""
+"""Adapter Array"""
 DAY: int = 10
 
 from utilities import load_data
-from copy import deepcopy
+from functools import reduce 
+import operator
 
 # Import -------------------------------------
 
@@ -32,188 +33,48 @@ def list_differences(x: list) -> list:
     return output
 
 pt1: list = list_differences(data)
+
     
 # Solve Part 2 -------------------------------
 
-def count_branches_group(point: int, x: list) -> int:
-    """Given a choice point in a list, count how many
-    downstream branches are created, recursively until end of conditional group. 
-    Output n branches. [Next, apply this function to each group in list]"""
+# Combinations don't multiply past the gaps of 3. Those gaps separate the full list into 
+# shorter groups. The number of combinations in the group is determined by its length. 
+# Combinations for the whole list are the product of all the group combinations. 
 
-    index: int = x.index(point)
-    remaining: list = x[index + 1:] 
-    output: int = 0
-    
-    choices: list = []
-    for item in remaining: 
-        if item - point <= 3: 
-            choices.append(item)
-            output += 1
-        else:
-            break
+def find_group_lengths(x: list) -> list:
+    """Given list of adapters, split into groups every time there is a gap of 3. 
+    Return a list of the length of each group"""
 
-    # if choices: 
-    #     for choice in choices: 
-    #         new: int = count_branches_group(choice, x)
-    #         output += new - 1
-    
-    return choices
-
-# def choose_next(current: int, full: list) -> list:
-#     """Given current position in list of adapters, list all possible options
-#     for the next step. Original list must be sorted first. NOT RECURSIVE"""
-
-#     index: int = full.index(current)
-#     remaining: list = full[index + 1:]
-#     possible: list = []
-#     diff: int = 0
-#     #counter: int = 0
-
-#     while diff < 4: 
-#         for item in remaining: 
-#             diff = item - current
-#             if diff < 4: 
-#                 possible.append(item)
-#                 #counter += 1
-#         # for adapter in possible: 
-#         #     choose_next(adapter, full)
-
-#     return possible
-
-# def count_solutions(current: int, full: list) -> int:
-#     """Given current value in (sorted) full list of adapters, counts all possible 
-#     options for the next step. Then runs recursively to count all options for every step 
-#     after that."""
-
-#     target: int = full[-1]
-#     index: int = full.index(current)
-#     remaining: list = full[index + 1:]
-#     counter: int = 0
-
-#     # base case
-#     if current == target: 
-#         return counter
-
-#     # recursive case
-#     possible: list = []
-#     for item in remaining: 
-#         diff = item - current
-#         if diff > 3: 
-#             break
-#         else: 
-#             possible.append(item)
-#             counter += 1
-#             # possible2: list = []
-#     for adapter in possible: 
-#         print(adapter)
-#         counter += count_solutions(adapter, full)
-
-#     return counter
-
-#     # THIS IS ON THE RIGHT TRACK, it finds all solutions, but not in a countable way
-#     # Somehow keep track of the decisions all the way along, return not a count but 
-#     # all combinations
-
-
-# def find_all_solutions_old(already: list, full: list) -> list: 
-#     """Given values already selected in full (sorted) list of adapters, finds all 
-#     possible options for the next step. Then runs recursively to find all options 
-#     for every step after that, all the way to finding all possible arrangements."""
-
-#     current: int = already[-1]
-#     index: int = full.index(current)
-#     remaining: list = full[index + 1:]
-#     output: list = []
-
-#     # base case  
-#     if already[-1] == full[-1]: 
-#         return already
-
-#     # recursive case
-#     possible_next: list = []
-#     for item in remaining: 
-#         if item - current <= 3: 
-#             possible_next.append(item)
-#         else:
-#             break       
-
-#     for adapter in possible_next: 
-#         new_arrange: list = already.copy()
-#         new_arrange.append(adapter)
-#         output.append(find_all_solutions(new_arrange, full))
-#         # solutions: list = find_all_solutions(new_arrange, full)
-#         # print(solutions)
-#         # for solution in solutions: 
-#         #     print(solution)
-#         #     #output.append(solution)
-
-#     return output
-
-#     # This almost works
-
-
-def count_all_solutions_old(already: list, full: list) -> int: 
-    """Given values already selected in full (sorted) list of adapters, finds all 
-    possible options for the next step. Then runs recursively to find all options 
-    for every step after that, all the way to finding all possible arrangements."""
-
-    current: int = already[-1]
-    index: int = full.index(current)
-    remaining: list = full[index + 1:] # sorted(list(set(full).difference(set(already)))) #
+    output: list = []
     count: int = 0
-
-    # base case  
-    if already[-1] == full[-1]: 
+    
+    for i, item in enumerate(x[:-1]): 
         count += 1
-        return count
+        diff: int = x[i + 1] - item
+        if diff >= 3: 
+            output.append(count)
+            count: int = 0
 
-    # recursive case
-    possible_next: list = []
-    for item in remaining: 
-        if item - current <= 3: 
-            possible_next.append(item)
-        else:
-            break       
+    return output 
 
-    for adapter in possible_next: 
-        new_arrange: list = already.copy()
-        new_arrange.append(adapter)
-        count += count_all_solutions(new_arrange, full)
+def count_combinations(group_len: int) -> int: 
+    """Given a group length, return the number of combinations. After the first 3, 
+    it's the sum of the previous 3."""
 
-    return count
+    sequence: list = [1, 1, 2]
 
-# STILL TOO SLOW. See if it's better to only pass current, not already
+    if group_len < 1: 
+        raise ValueError ("Length must be greater than 0")
+    if group_len <= 3: 
+        return sequence[group_len - 1]
+    else: 
+        while len(sequence) < group_len: 
+            next_item: int = sum(sequence[-3:])
+            sequence.append(next_item)
+        return sequence[-1]
 
-def count_all_solutions(current: int, full: list) -> int: 
-    """Given values already selected in full (sorted) list of adapters, finds all 
-    possible options for the next step. Then runs recursively to find all options 
-    for every step after that, all the way to finding all possible arrangements."""
-
-    index: int = full.index(current)
-    remaining: list = full[index + 1:] 
-    
-    # base case  
-    if current == full[-1]: 
-        return 1
-
-    # recursive case
-    count: int = 0
-    possible_next: list = []
-
-    for item in remaining: 
-        if item - current <= 3: 
-            possible_next.append(item)
-        else:
-            break       
-
-    for choice in possible_next: 
-        count += count_all_solutions(choice, full)
-
-    return count
-
-# I got it! First identify the points at which there is a choice. 
-# Count the options at each choice point. 
-# Multiply all options from all choice points. 
+group_lengths: list = find_group_lengths(data)
+combos: list = [count_combinations(group) for group in group_lengths]
 
 
 # Output -------------------------------------
@@ -223,8 +84,5 @@ if __name__ == "__main__":
     solution_1: int = pt1.count(1) * pt1.count(3)
     print(f'Part 1: {solution_1}')
 
-    test1 = count_branches_group(4, samp1)
-
-    #print(samp1)
-    solution_2 = test1
+    solution_2: int = reduce(operator.mul, combos)
     print(f'Part 2: {solution_2}')
